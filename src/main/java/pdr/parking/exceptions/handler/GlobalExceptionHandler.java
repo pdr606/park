2a808c4458d6cf02c6bcbbcc9251ffc75389b703
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-10-11 14:32:29 -0300

    Update GlobalExceptionHandler

--- src/main/java/pdr/parking/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/pdr/parking/exceptions/handler/GlobalExceptionHandler.java
@@ -1,33 +1,46 @@
 package pdr.parking.exceptions.handler;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pdr.parking.entities.User;
 import pdr.parking.exceptions.UserDuplicateException;
 import pdr.parking.exceptions.UserNotFoundException;
+import pdr.parking.exceptions.VehicleNotFoundException;
 
+import java.time.Instant;
 import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
     @ExceptionHandler(UserDuplicateException.class)
-    public ResponseEntity<Map<String, List<String>>> handleUserDuplicated(Exception ex){
-        List<String> err = Collections.singletonList(ex.getMessage());
-        return  ResponseEntity.ok(getErrorsMap(err));
+    public StandardError userDuplicateException(UserDuplicateException e, HttpServletRequest request){
+        String error = "User duplicate";
+        HttpStatus status = HttpStatus.CONFLICT;
+        return new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                e.getMessage(),
+                request.getRequestURI()
+        );
     }
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, List<String>>> handleUserNotFound(Exception ex){
-        List<String> err = Collections.singletonList(ex.getMessage());
-        return  ResponseEntity.ok(getErrorsMap(err));
-    }
-
-    private Map<String, List<String>> getErrorsMap(List<String> errors) {
-        Map<String, List<String>> errorsMap = new HashMap<>();
-        errorsMap.put("errors", errors);
-        return errorsMap;
+    @ExceptionHandler({UserNotFoundException.class, VehicleNotFoundException.class})
+    public StandardError UserNotFoundException(UserNotFoundException e, HttpServletRequest request){
+        String error = "Request not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                e.getMessage(),
+                request.getRequestURI()
+        );
     }
 }
